b2bf27cdaf3330c84f62757d4d5a067f445c7bdd
stefphf <stepan_tsoy500@mail.ru>
2022-12-18 03:25:00 +0300

    изменен шифр Виженера

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -16,7 +16,7 @@ def is_prime(n: int) -> bool:
         return True
     if n % 2 == 0 or n < 2:
         return False
-    for i in range(3, int(n ** 0.5) + 1, 2):
+    for i in range(3, int(n**0.5) + 1, 2):
         if n % i == 0:
             return False
     return True
