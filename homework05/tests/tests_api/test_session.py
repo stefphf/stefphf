b856fdfe66931e71fa187980c4b522717956cc9e
stefphf <stepan_tsoy500@mail.ru>
2022-12-21 06:54:03 +0300

    vkapi

--- homework05/tests/tests_api/test_session.py
+++ homework05/tests/tests_api/test_session.py
@@ -3,8 +3,8 @@ import unittest
 
 import httpretty
 import responses
-from requests.exceptions import ConnectionError, HTTPError, ReadTimeout, RetryError
-
+from requests.exceptions import (ConnectionError, HTTPError, ReadTimeout,
+                                 RetryError)
 from vkapi.session import Session
 
 
