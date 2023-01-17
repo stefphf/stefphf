4142ccefabdec1bad0280a8f8fddbb2ea59bc97a
stefphf <stepan_tsoy500@mail.ru>
2023-01-17 12:51:58 +0300

    vkapi

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -5,3 +5,4 @@ VK_CONFIG = {
     "access_token": "vk1.a.49WqmD6T3Mu7javINdoK_BjM8lvWtbETC5Uzf4o_cvnVeT91TaqvR67eTjaRBUqUKyU0cuAm7alwimHJyp5jgGUfOA0Xclt-7XlEKxk7V_d-u2wGIZ6Pf2zmnAqlb_y_3dkYO6JmLpFsXlkUbisjLd4Tu7IUSDrq9MiNf7Py-Oawbo_qbkCtk0zJYCeJankO&expires_in=86400",
     "version": "5.126",
 }
+
